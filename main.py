37b1dfd06701521e5e72f877e5becc6f76d523dd
wahyudi <wahyudi.coding@gmail.com>
2020-06-13 15:25:26 +0700

    add ball bounce etc

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ sprite_group.add(paddle)
 sprite_group.add(ball)
 
 clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 60)
+score = 0
 
 running = True
 
@@ -43,8 +45,39 @@ while running:
     # screen background color
     screen.fill((0, 255, 0))
 
-    sprite_group.draw(screen)
+    # if the ball hit the right wall
+    if ball.rect.x >= screen.get_width():
+        ball.velocity[0] = -ball.velocity[0]
+
+    # if the ball hit the left wall
+    if ball.rect.x <= 0:
+        ball.velocity[0] = -ball.velocity[0]
+
+    # if the ball hit top wall
+    if ball.rect.y <= 0:
+        ball.velocity[1] = -ball.velocity[1]
     
+    #display the score
+    
+    score_text = font.render(str(score), 1, (255, 255, 255))
+    screen.blit(score_text, (20, 20))
+
+    # if the ball hit the bottom
+    if ball.rect.y >= screen.get_height():
+        gameover_text = font.render("Game Over", 1, (255,0,0))
+        screen.blit(gameover_text, (screen.get_width()/2, screen.get_height()/2))
+        ball.stop()
+        print(score)
+
+    if pygame.sprite.collide_mask(ball, paddle):
+        score += 1
+        ball.bounce()
+
+    sprite_group.update()
+    sprite_group.draw(screen)
+
+
+
     # display the screen
     pygame.display.flip()
     clock.tick(60)
